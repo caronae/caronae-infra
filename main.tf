279aa66d682f37f6748bc486b72b950606197764
Mario Cecchi <macecchi@gmail.com>
2017-10-19 00:21:48 -0200

    Create secondary dev environment

--- main.tf
+++ main.tf
@@ -28,9 +28,10 @@ module "iam" {
   source = "./iam"
 }
 
-module "backend" {
+module "backend_prod" {
   source = "./backend"
 
+  environment          = "prod"
   api_domain           = "api.${terraform.workspace}.${var.domain}"
   ufrj_domain          = "ufrj.${terraform.workspace}.${var.domain}"
   site_domain          = "${terraform.workspace}.${var.domain}"
@@ -40,12 +41,35 @@ module "backend" {
   subnet               = "${module.network.subnet}"
 }
 
-module "dns" {
+module "backend_dev" {
+  source = "./backend"
+
+  environment          = "dev"
+  api_domain           = "api.dev.${terraform.workspace}.${var.domain}"
+  ufrj_domain          = "ufrj.dev.${terraform.workspace}.${var.domain}"
+  site_domain          = "dev.${terraform.workspace}.${var.domain}"
+  region               = "${var.region}"
+  security_group       = "${module.network.web_security_group}"
+  iam_instance_profile = "${module.iam.instance_iam_profile}"
+  subnet               = "${module.network.subnet}"
+}
+
+module "dns_prod" {
   source = "./dns"
 
   domain              = "${var.domain}"
   api_domain          = "api.${terraform.workspace}"
   ufrj_domain         = "ufrj.${terraform.workspace}"
   site_domain         = "${terraform.workspace}"
-  backend_instance_ip = "${module.backend.instance_ip}"
+  backend_instance_ip = "${module.backend_prod.instance_ip}"
+}
+
+module "dns_dev" {
+  source = "./dns"
+
+  domain              = "${var.domain}"
+  api_domain          = "api.dev.${terraform.workspace}"
+  ufrj_domain         = "ufrj.dev.${terraform.workspace}"
+  site_domain         = "dev.${terraform.workspace}"
+  backend_instance_ip = "${module.backend_dev.instance_ip}"
 }
