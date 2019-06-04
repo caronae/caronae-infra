c3a7072e23a279f4d1fb2323de5d44e337757bb3
Lucas Hanke <lhanke@thoughtworks.com>
2019-06-04 11:15:14 -0300

    upgrades storage module to terraform 0.12

--- storage/main.tf
+++ storage/main.tf
@@ -1,13 +1,13 @@
 data "template_file" "bucket_suffix" {
-  template = "${ terraform.workspace == "default" ? "" : "-${terraform.workspace}" }"
+  template = terraform.workspace == "default" ? "" : "-${terraform.workspace}"
 }
 
 resource "aws_s3_bucket" "user_content" {
   bucket = "usercontent${data.template_file.bucket_suffix.rendered}.caronae"
   acl    = "public-read"
 
-  tags {
-    Workspace = "${terraform.workspace}"
+  tags = {
+    Workspace = terraform.workspace
   }
 }
 
@@ -15,13 +15,13 @@ resource "aws_s3_bucket" "backups" {
   bucket = "backups${data.template_file.bucket_suffix.rendered}.caronae"
   acl    = "private"
 
-  tags {
-    Workspace = "${terraform.workspace}"
+  tags = {
+    Workspace = terraform.workspace
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "backups" {
-  bucket = "${aws_s3_bucket.backups.id}"
+  bucket = aws_s3_bucket.backups.id
 
   block_public_acls   = true
   block_public_policy = true
@@ -29,9 +29,9 @@ resource "aws_s3_bucket_public_access_block" "backups" {
 }
 
 output "user_content_bucket_arn" {
-  value = "${aws_s3_bucket.user_content.arn}"
+  value = aws_s3_bucket.user_content.arn
 }
 
 output "backups_bucket_arn" {
-  value = "${aws_s3_bucket.backups.arn}"
+  value = aws_s3_bucket.backups.arn
 }
