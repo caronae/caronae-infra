91524b2f6dc7545eadd22beddb4d1d919410f2d5
Lucas Hanke <lhanke@thoughtworks.com>
2019-06-04 11:12:10 -0300

    upgrade network module to terraform 0.12

--- network/main.tf
+++ network/main.tf
@@ -1,4 +1,5 @@
-variable "region" {}
+variable "region" {
+}
 
 variable "elastic_ip_count" {
   default = 2
@@ -7,55 +8,55 @@ variable "elastic_ip_count" {
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags {
+  tags = {
     Name = "caronae-${terraform.workspace}"
   }
 }
 
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags {
+  tags = {
     Name = "caronae-igw-${terraform.workspace}"
   }
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.default.id}"
+    gateway_id = aws_internet_gateway.default.id
   }
 
-  tags {
+  tags = {
     Name = "public-${terraform.workspace}"
-    env  = "${terraform.workspace}"
+    env  = terraform.workspace
   }
 }
 
 resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.vpc.id}"
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.region}a"
 
-  tags {
+  tags = {
     Name = "caronae-default-${terraform.workspace}"
   }
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = "${aws_subnet.default.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  subnet_id      = aws_subnet.default.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "web" {
   name        = "caronae-web"
   description = "Web server security group"
-  vpc_id      = "${aws_vpc.vpc.id}"
+  vpc_id      = aws_vpc.vpc.id
 
-  tags {
+  tags = {
     Name = "caronae-http-ssh-${terraform.workspace}"
   }
 
@@ -103,9 +104,9 @@ resource "aws_security_group" "web" {
 }
 
 output "subnet" {
-  value = "${aws_subnet.default.id}"
+  value = aws_subnet.default.id
 }
 
 output "web_security_group" {
-  value = "${aws_security_group.web.id}"
+  value = aws_security_group.web.id
 }
